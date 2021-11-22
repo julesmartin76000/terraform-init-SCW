76bee28aeeed815574082da05eb89040fba2f6ff
Jules Martin <julesmartin76000@gmail.com>
2021-11-22 15:40:03 +0100

    Create main for the terraform project

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "scaleway_instance_ip" "public_ip" {}
+
+resource "scaleway_instance_volume" "data" {
+  size_in_gb = 30
+  type       = "l_ssd"
+}
+
+resource "scaleway_instance_server" "my-instance" {
+  type  = "DEV1-L"
+  image = "ubuntu_focal"
+
+  tags = ["terraform instance", "my-instance"]
+
+  ip_id = scaleway_instance_ip.public_ip.id
+
+  additional_volume_ids = [scaleway_instance_volume.data.id]
+
+  root_volume {
+    # The local storage of a DEV1-L instance is 80 GB, subtract 30 GB from the additional l_ssd volume, then the root volume needs to be 50 GB.
+    size_in_gb = 50
+  }
+}
